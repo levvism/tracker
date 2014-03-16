bc768ae46eeeb29659ca6abff1bfa5a6e2e611d4
Anton Belev <belevanton@gmail.com>
2014-03-16 17:07:02 +0000

    Old code folder removed.

--- code/tracker_project/webapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from webapp.models import Project,Task,UserProfile,UsersProjects
-
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(UserProfile)
-admin.site.register(UsersProjects)
